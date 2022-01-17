7c1b6c49353e11a15935de28716c69fc90a78b7e
IvanAbuzimbale <58472137+IvanAbuzimbale@users.noreply.github.com>
2022-01-17 09:25:38 +0100

    Add files via upload

---
+++ PosudbaKnjigaIvanMočilac20122012/PosudbaKnjigaIvanMočilac20122012/Knjige.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PosudbaKnjigaIvanMočilac20122012
+{
+    class Korisnici
+    {
+        int isbn;
+        string naziv;
+        string autor;
+
+        public Korisnici(int isbn, string naziv, string autor)
+        {
+            this.isbn = isbn;
+            this.naziv = naziv;
+            this.autor = autor;
+        }
+
+        public int Isbn { get => isbn; set => isbn = value; }
+        public string Naziv { get => naziv; set => naziv = value; }
+        public string Autor { get => autor; set => autor = value; }
+    }
+}
---
+++ PosudbaKnjigaIvanMočilac20122012/PosudbaKnjigaIvanMočilac20122012/NovaKnjiga.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace PosudbaKnjigaIvanMočilac20122012
+{
+    public partial class NovaKnjiga : Form
+    {
+        public NovaKnjiga()
+        {
+            InitializeComponent();
+        }
+
+        private void btnUnesiKnjigu_Click(object sender, EventArgs e)
+        {
+            XDocument doc = new XDocument(
+                new XElement("Knjige",
+                    new XElement("ISBN", txtISBN.Text),
+                    new XElement("Naziv", txtNaziv.Text),
+                    new XElement("Autor", txtAutor.Text)
+                )
+            );
+            doc.Save("Knjige.xml");
+        }
+    }
+}
